32ececbd44a854f2b2f21c1c2181aa3d220eca2a
Yuntian Yang <yuntian_yang@brown.edu>
2023-11-20 20:55:31 -0500

    add movie getters

--- movie/movie.py
+++ movie/movie.py
@@ -1,6 +1,7 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, Http404
 
 from rating_backend.settings import ARCHIVE_DATA_FOLDER
+from django.db.models import Count
 from .models import Movie, Rating
 import csv
 
@@ -11,7 +12,7 @@ def load_csv_data(request):
         line_count = 0
         for row in csv_reader:
             if line_count % 10000 == 0:
-                print(f'Processed {line_count} lines.')
+                print(f'Processed {line_count} lines of movies.')
             if line_count == 0:
                 print(f'Column names are {", ".join(row)}')
                 line_count += 1
@@ -25,15 +26,38 @@ def load_csv_data(request):
         line_count = 0
         for row in csv_reader:
             if line_count % 10000 == 0:
-                print(f'Processed {line_count} lines.')
+                print(f'Processed {line_count} lines of ratings.')
             if line_count == 0:
                 print(f'Column names are {", ".join(row)}')
                 line_count += 1
             else:
                 if int(row[2]) > 500:
                     break
-                m = Rating(movie_id=row[2], user_id=row[0], rating=row[1])
+                m = Rating(movie_id=Movie(id=int(row[2])), user_id=row[0], rating=row[1])
                 m.save()
                 line_count += 1
         print(f'Processed {line_count} lines for rating table.')
     return JsonResponse({"msg": "load success"})
+
+def get_movie(request, id):
+    try:
+        movie = Movie.objects.get(pk=id)
+        return JsonResponse({
+            'id': movie.id,
+            'year': movie.year,
+            'name': movie.name
+        })
+    except Movie.DoesNotExist:
+        raise Http404("Movie does not exist")
+    
+def get_movie_rating_dist(request):
+    # Assume movie ID is provided as a query parameter
+    movie_id = request.GET.get('movie_id')
+    if not movie_id:
+        return JsonResponse({'error': 'Movie ID is required.'}, status=400)
+    
+    distribution = Rating.objects.filter(movie_id=int(movie_id)).values('rating').annotate(count=Count('rating')).order_by('rating')
+    print(distribution)
+    distribution_data = {rating['rating']: rating['count'] for rating in distribution}
+    
+    return JsonResponse(distribution_data)
--- movie/urls.py
+++ movie/urls.py
@@ -4,4 +4,6 @@ from . import movie
 
 urlpatterns = [
     path("load/", movie.load_csv_data, name="load csv data"),
+    path("<int:id>/", movie.get_movie, name="get movie details"),
+    path("rating_distribution/", movie.get_movie_rating_dist, name="get movie rating distribution")
 ]
