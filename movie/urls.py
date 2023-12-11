14e9e470e5b701993d8da3dbb3278aac20906408
ZhangYW18 <yiweizh18@gmail.com>
2023-12-11 08:04:16 +0800

    update chart formats

--- movie/urls.py
+++ movie/urls.py
@@ -10,10 +10,10 @@ urlpatterns = [
     path("<int:id>/", movie.get_movie, name="get movie details"),
     path("get_rating_distribution/<int:movie_id>/", movie.get_movie_rating_dist, name="get movie rating distribution"),
     # Get noise pie chart for all noised ratings
-    path("generate_noise_bar_diagram/", movie.get_noise_pie_chart_for_movie),
+    path("generate_noise_pie_diagram/", movie.get_noise_pie_chart_for_movie),
     # Get noise pie chart for noised ratings of a single movie
-    path("generate_noise_bar_diagram/<int:movie_id>/", movie.get_noise_pie_chart_for_movie),
-    path("generate_avg_noise_diagram/<int:movie_id>/", movie.get_avg_noise_trend_line_chart_for_movie),
+    path("generate_noise_pie_diagram/<int:movie_id>/", movie.get_noise_pie_chart_for_movie),
+    path("generate_noise_trend_diagram/<int:movie_id>/", movie.get_avg_noise_trend_line_chart_for_movie),
     path("generate_avg_noises_diagram_all_movies/", movie.get_avg_noise_scatter_chart_for_all_movies),
     path("generate_avg_noises_diagram_all_users/", movie.get_avg_noise_scatter_chart_for_all_users),
     path("generate_variance_diagram_all_movies/", movie.get_variance_scatter_chart_all_movies),
